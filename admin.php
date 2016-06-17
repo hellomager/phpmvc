fbfd26b3bbbc6bff0989c7db2d0c76f8ce1e9430
hellomager <hellomager@163.com>
2016-06-17 14:12:07 +0800

    add error_reporting

--- admin.php
+++ admin.php
@@ -2,6 +2,7 @@
 	header("Content-type: text/html; charset=utf-8");
 	//url形式  index.php?controller=控制器名&method=方法名
 	error_reporting(E_ALL^E_NOTICE^E_WARNING^E_DEPRECATED);
+
 	require_once('config.php');
 	require_once('framework/pc.php');
 	PC::run($config);
